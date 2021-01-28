c06532ae73ad3106da6d4713e0af717874af6002
damir <damir@gismeteo.ru>
2021-01-28 16:20:51 +0300

    Homework-2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import logo from './logo.svg';
+import Header from './components/Header';
+import Layout from './components/Layout';
+import Footer from './components/Footer';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, React.js!
-        </p>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Layout
+        id="1"
+        title="Title 1"
+        desc="Description 1"
+      />
+      <Layout
+        id="2"
+        title="Title 2"
+        desc="Description 2"
+      />
+      <Layout
+        id="3"
+        title="Title 3"
+        desc="Description 3"
+      />
+      <Footer />
+    </>
   );
 }
 
